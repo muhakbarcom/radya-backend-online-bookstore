93611b0a5b997d44a9fe7d4a92d68650882efe4f
muhakbarcom <muhammad.akbar5999@gmail.com>
2024-06-16 13:01:13 +0700

    Book - Fix namespace model

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use app\Models\Book;
+use App\Models\Book;
 
 class BookSeeder extends Seeder
 {
